8e2b8b90c874d671c5cc846d99ad04250a6f38f4
Harpreet Dhanda <harpreet_h@hotmail.co.uk>
2022-07-20 15:33:38 +0100

    Added in the list index forms

--- controllers/controller.py
+++ controllers/controller.py
@@ -5,3 +5,9 @@ from models.orderlist import orders
 @app.route("/orders")
 def index():
     return render_template("index.html", title="Home", all_orders=orders)
+
+@app.route("/orders/<index>")
+def singleorder(index):
+    index = int(index)
+    return render_template("singleorder.html", title="Order Outstanding", order=orders[index])
+
